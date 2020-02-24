e2534ec84b0d353e0fa11fb805ce3b20ac019f61
yseul <qlcid@naver.com>
2020-02-24 19:54:35 +0900

    File upload and Insert data into Mysql

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -56,6 +57,7 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
+      <div>
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -81,6 +83,8 @@ class App extends Component {
           </TableBody>
         </Table>
       </Paper>
+      <CustomerAdd/>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const mysql = require('mysql');
 const data = fs.readFileSync('./config.json');
 const conf = JSON.parse(data);
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' });
+
 const connection = mysql.createConnection({
     host: conf.host,
     user: conf.username,
@@ -22,13 +25,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/customers', (req, res) => {
-    setTimeout(function() { console.log("for test") }, 500);
+    // setTimeout(function() { console.log("for test") }, 500);
     connection.query(
         'SELECT * FROM CUSTOMERS;',
         (err, rows, fields) => {
             res.send(rows);
         }
-    )
+    );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMERS VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
